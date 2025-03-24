8944f56248fa7002ac890f6bcc1f75e62ca83462
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-24 21:39:43 +0600

    Solve ArraySum problem

---
+++ src/ArraySum/Program.cs
@@ -0,0 +1,15 @@
+﻿var arCount = Convert.ToInt32(Console.ReadLine()?.Trim());
+
+var ar = Console.ReadLine()?.TrimEnd().Split(' ').ToList().Select(int.Parse).ToList();
+
+if (ar is not null)
+{
+    var result = SimpleArraySum(ar);
+    Console.WriteLine(result);
+}
+
+static int SimpleArraySum(List<int> ar)
+{
+    return ar.Sum();
+}
+
