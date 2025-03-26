831a9370d8eacc863d4cc9e0d10d5d351ae790a3
Abdullah Bin Mamun <binbean44@gmail.com>
2025-03-26 17:29:31 +0600

    Solve Plus Minus Problem

---
+++ src/PlusMinus/Program.cs
@@ -0,0 +1,20 @@
+﻿var n = int.Parse(Console.ReadLine()?? string.Empty);
+
+var arr = (Console.ReadLine()?? string.Empty).Split(' ').Select(int.Parse).ToList();
+
+PlusMinus(arr);
+
+static void PlusMinus(List<int> arr)
+{
+    var count = arr.Count;
+
+	int positive = arr.Count(x => x > 0);
+    int negative = arr.Count(x => x < 0);
+    int zero = arr.Count(x => x == 0);
+
+
+    foreach (var item in new List<int> { positive, negative, zero })
+    {
+        Console.WriteLine(((double)item / count).ToString("0.000000"));
+    }
+}
